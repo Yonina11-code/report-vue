569edadb6609fe1f80f390e4d7be07902420622c
Yonina11-code <572391370@qq.com>
2022-01-27 16:26:13 +0800

    20220127

--- src/packages/common.js
+++ src/packages/common.js
@@ -4,9 +4,18 @@
  * 字符串模板的代替方案，允许你发挥 JavaScript 最大的编程能力。
  * 该渲染函数接收一个 createElement 方法作为第一个参数用来创建 VNode
  */
+import echartsLib from 'echarts/lib/echarts'
 import dataEmpty from '@/components/data-empty'
 import loading from '@/components/loading'
+import { debounce, getType, isArray, isObject } from 'utils-lite'
+import { STATIC_PROPS, DEFAULT_THEME, DEFAULT_COLORS, ECHARTS_SETTINGS } from './constants'
+import numerify from 'numerify'
+import setAnimation from './modules/animation'
+import setMark from './modules/mark'
+import setExtend from './modules/extend'
+
 export default {
+  _numerify: numerify,
   render (h) {
     return h('div', {
 
@@ -75,8 +84,184 @@ export default {
     notSetUnchange: Array,
     log: Boolean
   },
+  watch: {
+    data: {
+      handler (v) {
+        if (v) {
+          this.changeHandler()
+        }
+      },
+      deep: true
+    },
+    settings: {
+      deep: true,
+      handler (v) {
+        if (v.type && this.echartsLib) this.chartHandler = this.echartsLib[v.type]
+        this.changeHandler()
+      }
+    },
+    width: 'nextTickResize',
+    height: 'nextTickResize',
+    events: {
+      handler: 'createEventProxy',
+      deep: true
+    },
+    theme: {
+      handler: 'themeChange',
+      deep: true
+    },
+    themeName: 'themeChange',
+    resizeable: 'resizeableHandler'
+  },
+  computed: {
+    canvasStyle () {
+      return {
+        width: this.width,
+        height: this.height,
+        position: 'relative'
+      }
+    },
+    chartColor () {
+      return this.colors || (this.theme && this.theme.color) || DEFAULT_COLORS
+    }
+  },
   created () {
     // TODO
     this.echarts = null
+    this.registeredEvents = [] // 注册事件集合
+    this._once = {}
+    this._store = {}
+    this.resizeHandler = debounce(this.resize, this.resizeDelay) // 重置页面操作
+    this.changeHandler = debounce(this.dataHandler, this.changeDelay) // 数据改变时的操作
+    this.addWatchToProps() // 将props添加watch
+  },
+  mounted () {
+    this.init() // dom加载完成，初始化
+  },
+  beforeDestroy () {
+    this.clean() // 将要离开页面时，销毁页面的监听事件
+  },
+  methods: {
+    dataHandler () {
+      if (!this.chartHandler) return
+      let data = this.data
+      const { columns = [], rows = [] } = data
+      const extra = {
+        tooltipVisible: this.tooltipVisible,
+        legendVisible: this.legendVisible,
+        echarts: this.echarts,
+        color: this.chartColor,
+        tooltipFormatter: this.tooltipFormatter,
+        _once: this._once
+      }
+      if (this.beforeConfig) data = this.beforeConfig(data)
+      let options = this.chartHandler(columns, rows, this.settings, extra)
+      if (options) {
+        if (typeof options.then === 'function') {
+          options.then(this.optionsHandler)
+        } else {
+          this.optionsHandler(options)
+        }
+      }
+    },
+    optionsHandler (options) {
+      // legend图例，展现了不同系列的标记、颜色和名字
+      if (this.legendPosition && options.legend) {
+        options.legend[this.legendPosition] = 10
+        if (~['left', 'right'].indexOf(this.legendPosition)) {
+          options.legend.top = 'middle'
+          options.legend.orient = 'vertical'
+        }
+      }
+      // 主题色
+      options.color = this.chartColor
+      //
+      ECHARTS_SETTINGS.forEach(setting => {
+        if (this[setting]) options[setting] = this[setting]
+      })
+      // animation
+      if (this.animation) setAnimation(options, this.animation)
+      // marks: 标记点
+      if (this.markArea || this.markLine || this.markPoint) {
+        const marks = {
+          markArea: this.markArea,
+          markLine: this.markLine,
+          markPoint: this.markPoint
+        }
+        const series = options.series
+        if (isArray(series)) {
+          series.forEach(item => { setMark(item, marks)})
+        } else if (isObject(series)) {
+          setMark(series, marks)
+        }
+      }
+      if (this.extend) setExtend(options, this.extend)
+    },
+    addWatchToProps () {
+      const watchedVariable = this._watchers.map(watcher => watcher.expression)
+      Object.keys(this.$props).forEach(prop => {
+        // ~按位非：对数值取反并减1
+        if (!~watchedVariable.indexOf(prop) && !~STATIC_PROPS.indexOf(prop)) {
+          const opts = {}
+          if (~['[object Object]', '[object Array]'].indexOf(getType(this.$props[prop]))) {
+            opts.deep = true
+          }
+          this.$watch(prop, () => {
+            this.changeHandler()
+          }, opts)
+        }
+      })
+    },
+    init () {
+      if (this.echarts) return
+      const themeName = this.themeName || this.theme || DEFAULT_THEME
+      this.echarts = echartsLib.init(this.$refs.canvas, themeName, this.initOptions)
+      if (this.data) this.changeHandler()
+      this.createEventProxy() // 调用事件代理
+      if (this.resizeable) this.addResizeListener()
+    },
+    createEventProxy () {
+      // 只要用户使用on方法绑定的时间都做一层代理
+      // 是否真正执行相应的事件方法取决于该方法是否仍然存在events中
+      // 实现events的动态响应
+      const self = this
+      const keys = Object.keys(this.events || {})
+      keys.length && keys.forEach(ev => {
+        if (this.registeredEvents.indexOf(ev) === -1) {
+          this.registeredEvents.push(ev)
+          this.echarts.on(ev, (function (ev) {
+            return function (...args) {
+              if (ev in self.events) {
+                self.events[ev].apply(null, args)
+              }
+            }
+          })(ev))
+        }
+      })
+    },
+    addResizeListener () {
+      // 页面重置时，重置图表
+      window.addEventListener('resize', this.resizeHandler)
+      this._once.onresize = true
+    },
+    clean () {
+      if (this.resizeable) this.removeResizeListener()
+      this.echarts.dispose()
+    },
+    removeResizeListener () {
+      window.removeEventListener('resize', this.resizeHandler)
+      this._once.onresize = false
+    },
+    //
+    nextTickResize () {
+      // 等待页面更新完成重置
+      this.$nextTick(this.resize)
+    },
+    // 更改主题
+    themeChange (theme) {
+      this.clean()
+      this.echarts = null
+      this.init()
+    }
   }
 }
---
+++ src/packages/constants.js
@@ -0,0 +1,37 @@
+export const STATIC_PROPS = [
+  'initOptions', 'loading', 'dataEmpty',
+  'judgeWidth', 'widthChangeDelay'
+]
+export const DEFAULT_THEME = {
+  categoryAxis: {
+    axisLine: { show: false },
+    axisTick: { show: false },
+    splitLine: { show: false }
+  },
+  valueAxis: {
+    axisLine: { show: false }
+  },
+  line: {
+    smooth: true
+  },
+  grid: {
+    containLabel: true,
+    left: 10,
+    right: 10
+  }
+}
+export const DEFAULT_COLORS = [
+  '#19d4ae', '#5ab1ef', '#fa6e86',
+  '#ffb980', '#0067a6', '#c4b4e4',
+  '#d87a80', '#9cbbff', '#d9d0c7',
+  '#87a997', '#d49ea2', '#5b4947',
+  '#7ba3a8'
+]
+export const ECHARTS_SETTINGS = [
+  'grid', 'dataZoom', 'visualMap',
+  'toolbox', 'title', 'legend',
+  'xAxis', 'yAxis', 'radar',
+  'tooltip', 'axisPointer', 'brush',
+  'geo', 'timeline', 'graphic',
+  'series', 'backgroundColor', 'textStyle'
+]
